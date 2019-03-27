5e1bd78ee7bd95b709d22c4f008ece8d7000b58c
Shigma <1700011071@pku.edu.cn>
2019-03-27 18:19:23 +0800

    generate page keys depending on relative path

--- __tests__/docs/.vuepress/config.js
+++ __tests__/docs/.vuepress/config.js
@@ -1,10 +1,12 @@
 const { resolve } = require('path')
+const { hash } = require('@vuepress/shared-utils')
 const CSSExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   dest: resolve(__dirname, '../../dist'),
 
   plugins: [
+    // do not use SSR in 404.html
     [require('vuepress-plugin-dehydrate')],
     [require('../../..'), {
       pageChunkName ({ key, frontmatter }) {
@@ -17,12 +19,19 @@ module.exports = {
     }],
   ],
 
+  extendPageData (page) {
+    // generate page keys depending on relative path
+    page.key = 'v-' + hash(page.path)
+  },
+
   evergreen: false,
 
   chainWebpack (config) {
+    // do not use chunk hash in js
     config
       .output.filename('assets/js/[name].js')
 
+    // do not use chunk hash in css
     config
       .plugin('extract-css')
       .use(CSSExtractPlugin, [{
